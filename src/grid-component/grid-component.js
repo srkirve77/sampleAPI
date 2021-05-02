065791450e0411aa51a9b193b7d10ee790d4c064
Shubham Kirve <shubhamkirve@Shubhams-MacBook-Pro.local>
2021-05-02 18:19:05 +0530

    added filter for status and gender

--- src/grid-component/grid-component.js
+++ src/grid-component/grid-component.js
@@ -12,6 +12,7 @@ import { useState, useEffect } from 'react';
 import AddUser from '../adduser-component/adduser-component'
 import './grid-component.css';
 import UpdateUser from '../updateuser-component/updateuser-component';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const GridComponent= () => {
     const [showAddUser, setShowAddUser] = useState(false)
@@ -19,12 +20,36 @@ const GridComponent= () => {
     const [Users, setUsers]= useState([])
     const [pageNo, setPageNo] = useState(1)
     const [userToUpdate, setUserToUpdate] = useState('')
+    const [maleFilter, setMaleFilter] = useState(false)
+    const [femaleFilter, setFemaleFilter] = useState(false)
+    const [activeFilter, setActiveFilter] = useState(false)
+    const [inactiveFilter, setInactiveFilter] = useState(false)
 
     useEffect(() => {
         const getUsers = async () => {
 
-            const users = await fetchUsers()
+            var users = await fetchUsers()
+            if(maleFilter===true && femaleFilter===true) {
+            }
+            else if(maleFilter===true) {
+                users = users.filter( user =>  user.gender === 'Male' )
+            }
+            else if(femaleFilter===true) {
+                users = (users.filter( user =>  user.gender === 'Female' ) )
+            }
+            
+
+            if(activeFilter===true && inactiveFilter===true) {
+            }
+            else if(activeFilter===true) {
+                users = (users.filter( user =>  user.status === 'Active' ) )
+            }
+            else if(inactiveFilter===true) {
+                users = (users.filter( user =>  user.status === 'Inactive' ) )
+            }
+            
             setUsers(users)
+
         }
             
         getUsers()
@@ -100,8 +125,6 @@ const GridComponent= () => {
                 body: JSON.stringify(user),
             })
         const data = await res.json()
-        console.log(data)
-        console.log(JSON.stringify(user))
         if(parseInt(data.code/100)!==2) {
             var i;
             var msg = ""
@@ -112,12 +135,54 @@ const GridComponent= () => {
         }
     }
 
+    const filterMale = () => {
+        setMaleFilter(!maleFilter)
+    }
+
+    const filterFemale = () => {
+        setFemaleFilter(!femaleFilter)
+    }
+
+    const filterActive = () => {
+        setActiveFilter(!activeFilter)
+    }
+
+    const filterInactive = () => {
+        setInactiveFilter(!inactiveFilter)
+    }
+
     const onClickAddUser = () => {
         setShowAddUser(!showAddUser)
     }
 
     return (
         <Paper>
+            <div className="genderfilter">
+            <label className = "genderlabel">Gender</label>
+            <ul>
+                <li className="gender_option_label">
+                    <label>Male</label>
+                        <Checkbox color="primary" onClick = {filterMale} inputProps={{ 'aria-label': 'secondary checkbox' }}/>
+                </li>
+                <li className = "gender_option_label">
+                    <label>Female</label>
+                        <Checkbox color="primary" onClick = {filterFemale} inputProps={{ 'aria-label': 'secondary checkbox' }}/>
+                </li>
+            </ul>
+            </div>
+            <div className="statusfilter">
+            <label className = "statuslabel">Status</label>
+            <ul>
+                <li className="status_option_label">
+                    <label>Active</label>
+                        <Checkbox color="primary" onClick={filterActive} inputProps={{ 'aria-label': 'secondary checkbox' }}/>
+                </li>
+                <li className = "status_option_label">
+                    <label>Inactive</label>
+                        <Checkbox color="primary" onClick = {filterInactive} inputProps={{ 'aria-label': 'secondary checkbox' }}/>
+                </li>
+            </ul>
+            </div>
             <label className='pagelabel'>Page</label>
             <input className='pageinput' type="text" placeholder='1' onChange = { (e) => setPageNo(e.target.value)}/>
             { showUpdateUser && <UpdateUser style={{marginLeft:1000}} user = {userToUpdate} ToUpdate={updateServer}/>}
